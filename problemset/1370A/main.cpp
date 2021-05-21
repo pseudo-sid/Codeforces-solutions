f49897925cbcd8f89442bdbf31c67d3079471027
pseudo-sid <siddharth19111@iiitd.ac.in>
2021-05-21 17:54:31 +0530

    fastIO

--- problemset/1370A/main.cpp
+++ problemset/1370A/main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     /* code */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     int t;  cin >> t;
     while(t--){
         int n;  cin >> n;
