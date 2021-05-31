3c430ee48a6fcd086441d9f92b64735666618d0c
pseudo-sid <siddharth19111@iiitd.ac.in>
2021-05-31 12:31:17 +0530

    games

---
+++ problemset/630A/main.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    long long n;    cin >> n;
+    cout << 25;
+    return 0;
+}
---
+++ problemset/959A/main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+
+    long n; cin >> n;
+    if(n%2 == 0){
+        cout <<"Mahmoud";
+    }
+    else{
+        cout <<"Ehab";
+    }
+    return 0;
+}
