d9b70619c3ab072e4c1df7e264ce0fe78ff86cae
pseudo-sid <siddharth19111@iiitd.ac.in>
2021-06-01 12:49:25 +0530

    Ichihime

---
+++ problemset/1337A/main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;  cin >> t;
+    while(t--){
+        ll a;   cin >> a;
+        ll b;   cin >> b;
+        ll c;   cin >> c;
+        ll d;   cin >> d;
+
+        cout << a <<" " << c << " " << c  << "\n";
+    }
+    return 0;
+}
