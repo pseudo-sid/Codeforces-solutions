3cd61d86f4e16a929f3ab6987970ed7e77f0d38f
pseudo-sid <siddharth19111@iiitd.ac.in>
2021-06-01 21:17:50 +0530

    init

---
+++ problemset/1325A/main.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;  cin >> t;
+    while(t--){
+        ll x;   cin >> x;
+        cout << 1 <<" " << x-1 << "\n";
+    }
+    return 0;
+}
---
+++ problemset/1352C/main.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+ 
+int main(){
+    
+    int t;  cin >> t;
+    while(t--){
+        ll n;   cin >> n;
+        ll k;   cin >> k;
+        if(k <= n-1)
+            cout << k << "\n";
+        else{
+           ll c = k/(n-1);
+           if(k % (n-1) == 0)
+            c--;
+           ll numb_undivisible_covered = c * (n-1);
+           ll pointer = c * n;
+           ll reqd = k - numb_undivisible_covered;
+           cout << pointer + reqd <<"\n";
+        }
+    }
+    return 0;
+}
