fa12e21d2bfef42163d124cc7cc880b341f6d368
pseudo-sid <siddharth19111@iiitd.ac.in>
2021-06-05 08:10:24 +0530

    Vus the Cossack and a Contest

---
+++ problemset/1186A/main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int n;  cin >> n;
+    int m;  cin >> m;
+    int k;  cin >> k;
+    if(n <= m and n <= k)
+        cout << "YES";
+    else
+        cout <<"NO";
+    return 0;
+}
