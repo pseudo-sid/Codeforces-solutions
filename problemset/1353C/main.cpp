5b29e025fee888b56e8a1737eecae02ed3e22f92
pseudo-sid <siddharth19111@iiitd.ac.in>
2021-06-06 11:14:52 +0530

    1353c

--- problemset/1353C/main.cpp
+++ problemset/1353C/main.cpp
@@ -5,9 +5,10 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     /* code */
-    int t;  cin >> t;
+    long t;  cin >> t;
     while(t--){
-        int n;  cin >> n;
+        ll n;  cin >> n;
+        n /= 2;
         ll res = 8 * n * (n + 1) * (2 * n + 1) / 6;
         cout << res <<"\n";
     }
