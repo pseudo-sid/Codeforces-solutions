e5254eb5c2948849b0531400f0dacdabe4322604
pseudo-sid <siddharth19111@iiitd.ac.in>
2021-06-13 01:15:52 +0530

    Sort the Array (finished)

--- problemset/451B/main.cpp
+++ problemset/451B/main.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	/* code */
-	#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	#endif
+	// #ifndef ONLINE_JUDGE
+	// freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+	// #endif
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
 	int n;	cin >> n;
 	vector<int> a;
@@ -19,36 +21,38 @@ int main(int argc, char const *argv[])
 		cin >> x;
 		a.push_back(x);
 	}
-	bool dec = false;
-	int s = 1, e = 1, i = 1;
-	while(i < n){
-		if(dec and a[s] > a[i])
+	int j = 1, k = 1;
+	for(int i = 1; i < n; i++){
+		if(a[i] > a[i+1]){
+			j = i;
+			while(i < n and a[i] >= a[i+1])
+				i++;
+			k = i;
 			break;
-		if(a[i] <= a[i+1]){
-			i++;
-			continue;
-		}
-		else{
-			if(dec)
-				break;
-			else{
-				s = i;
-				while(i < n and a[i] >= a[i+1])
-					i++;
-				e = i;
-				dec = true;
-			}
 		}
 	}
+	int l = j, m = k;
+	while(j < k)
+		swap(a[j++], a[k--]);
+	
 
-
-	if(i < n)
-		cout <<"no";
-	else
+	bool flag = true;
+	for(int i = 1; i <n; i++)
 	{
-		cout <<"yes\n";
-		cout << s << " " << e;
+		if(a[i] > a[i+1])
+		{
+			flag = false;
+			break;
+		}
 	}
+	
+	if(flag){
+		cout << "yes\n";
+		cout << l <<" " << m;
+	}
+	else
+		cout <<"no";
+	
 	return 0;
 }
 
