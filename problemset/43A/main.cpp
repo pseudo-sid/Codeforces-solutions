be26509fa9bfb95a6d873c84209e0cb86c3be561
pseudo-sid <siddharth19111@iiitd.ac.in>
2021-06-13 07:25:02 +0530

    Football

---
+++ problemset/43A/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    string r, s;
+    int n;  cin >> n;
+    cin >> s;
+    string t;
+    int x = 1, y = 0;
+    n--;
+    while(n--){
+        cin >> t;
+        if(t == s)
+            x++;
+        else{
+            r = t;
+            y++;
+        }
+    }
+    if(x > y)
+        cout << s <<"\n";
+    else
+        cout << r <<"\n";
+    return 0;
+}
