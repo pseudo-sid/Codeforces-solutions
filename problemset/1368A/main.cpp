c026ebb8fa64bbb796e49bf7d60e96baded439da
pseudo-sid <siddharth19111@iiitd.ac.in>
2021-06-16 04:11:46 +0530

    C+=

--- problemset/1368A/main.cpp
+++ problemset/1368A/main.cpp
@@ -5,16 +5,25 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     /* code */
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+    
     int T;  cin >> T;
     while(T--){
-        ll a;   cin >> a;
-        ll b;   cin >> b;
-        ll n;   cin >> n;
-        ll s1 = 0, s2 = 0;
-
-        s1 = (n -a)/b + 1;
-        s2 = (n -b)/a + 1;
-        cout << min(s1,s2) << "\n";
+        ll a; cin >> a;
+        ll b; cin >> b;
+        ll n; cin >> n;
+        int ct = 0;
+        while(a <= n and b <= n){
+            if(a < b)
+                a += b;
+            else
+                b += a;
+            ct++;
+        }
+        cout << ct<<"\n";
     }
     return 0;
 }
