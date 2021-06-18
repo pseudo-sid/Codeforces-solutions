82b237aebd9da1bfc904092f9ed75e1aff1a1531
pseudo-sid <siddharth19111@iiitd.ac.in>
2021-06-19 05:29:49 +0530

    Filling Diamonds

---
+++ problemset/1339A/main.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;  cin >> t;
+    while(t--){
+        ll n;   cin >> n;
+        cout << n <<"\n";
+    }
+    return 0;
+}
